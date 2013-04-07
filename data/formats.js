434533a7449ee99654a27256489a44481c171fce
Oiawesome <Oiawesome99@yahoo.com>
2013-04-07 13:21:59 -0700

    Update formats.js

--- data/formats.js
+++ data/formats.js
@@ -27,18 +27,6 @@
                 ruleset: ['Pokemon', 'Standard', 'Evasion Abilities Clause', 'Team Preview','Sleep Clause'],
                 banlist: ['BL2', 'Abomasnow', 'Ambipom', 'Arcanine', 'Archeops', 'Azelf', 'Azumarill', 'Beheeyem', 'Bisharp', 'Blastoise', 'Bronzong', 'Chandelure', 'Claydol', 'Cobalion', 'Cofagrigus', 'Crobat', 'Dusclops', 'Empoleon', 'Flygon', 'Froslass', 'Heracross', 'Hitmontop', 'Honchkrow', 'Houndoom', 'Kingdra', 'Krookodile', 'Meloetta', 'Machamp', 'Mew', 'Mienshao', 'Milotic', 'Mismagius', 'Nidoking', 'Porygon-Z', 'Porygon2', 'Raikou', 'Registeel', 'Rhyperior', 'Roserade', 'Rotom-Heat', 'Sableye', 'Scrafty', 'Sharpedo', 'Shaymin', 'Slowbro', 'Snorlax', 'Suicune', 'Swampert', 'Togekiss', 'Tornadus', 'Umbreon', 'Victini', 'Virizion', 'Weavile', 'Xatu', 'Yanmega', 'Zapdos', 'Zoroark', 'Shell Smash + Baton Pass', 'BL1', 'OU','Uber'],
         },
-        uuv2: {
-                effectType: 'Format',
-                section:'Version 2 Tiers',
-                name: "uuv2",
-                mod: 'uuv2',
-                challengeShow: true,
-                searchShow: true,
-                isTeambuilderFormat: true,
-                rated: true,
-                ruleset: ['Pokemon', 'Standard', 'Evasion Abilities Clause', 'Team Preview','Sleep Clause'],
-                banlist: ['Archeops', 'Weavile', 'BL2', 'OU', 'Uber'],
-        },
 	randombattle: {
 		name: "Random Battle",
 		section: "Singles",
