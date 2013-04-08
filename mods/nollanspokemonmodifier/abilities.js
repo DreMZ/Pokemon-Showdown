90fb0e58ff11519a73e09e3fd39cbbdda7b923a8
Nollan101 <kgb@kgbudge.com>
2013-04-08 17:28:57 -0600

    Update abilities.js

--- mods/nollanspokemonmodifier/abilities.js
+++ mods/nollanspokemonmodifier/abilities.js
@@ -236,6 +236,7 @@ exports.BattleAbilities = {
         desc: "When this Pokemon enters the battlefield, it causes a 5-turn Gravity.",
         shortDesc: "On switch-in, this Pokemon summons Gravity for 5 turns.",
         this.debug("Starting Gravity");
+         onStart: function(pokemon) {
           if (this.pseudoWeather['gravity']) {
             this.removePseudoWeather('gravity', pokemon, pokemon);
         }
@@ -266,7 +267,7 @@ exports.BattleAbilities = {
   "ancientwind": {
         desc: "When this Pokemon enters the battlefield, it causes a 4-turn Tailwind.",
         shortDesc: "On switch-in, this Pokemon summons Tailwind for 4 turns.",
-        onStart: function(source) {
+        onStart: function(pokemon) {
          this.debug("Starting Tailwind");
         this.addPseudoWeather('tailwind', pokemon, pokemon);
         this.pseudoWeather['tailwind'].duration = 4;
