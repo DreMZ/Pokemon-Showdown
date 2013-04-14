7989d42a98434c7576d329cac9647e420f169c55
Nollan101 <kgb@kgbudge.com>
2013-04-13 19:25:39 -0600

    Update pokedex.js

--- mods/dscap/pokedex.js
+++ mods/dscap/pokedex.js
@@ -1,3 +1,3 @@
 exports.BattlePokedex = {
-missingno:{num:-100,species:"Electrokan.",types:["Electric","Fighting"],baseStats:{hp:103,atk:135,def:90,spa:55,spd:81,spe:86},abilities:{0:"Mold Breaker",DW:"Sheer Force"},heightm:4,weightkg:250.0,color:"Yellow",eggGroups:["Field","Humanshape"]},
+electrokan:{num:-100,species:"Electrokan",types:["Electric","Fighting"],baseStats:{hp:103,atk:135,def:90,spa:55,spd:81,spe:86},abilities:{0:"Mold Breaker",DW:"Sheer Force"},heightm:4,weightkg:250.0,color:"Yellow",eggGroups:["Field","Humanshape"]},
 };
