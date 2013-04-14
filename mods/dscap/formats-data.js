7f3e1e79bd89f755c05d0430c2976ff528c2eb20
Nollan101 <kgb@kgbudge.com>
2013-04-14 17:39:08 -0600

    Update formats-data.js

--- mods/dscap/formats-data.js
+++ mods/dscap/formats-data.js
@@ -1,6 +1,6 @@
 exports.BattleFormatsData = {
         electrokan: {
-                viableMoves: {"wildcharge":1,"closecombat":1,"superpower":1,"thunderpunch":1,"stoneedge":1,"acrobatics":1,"bulkup":1,"substitute":1,"focuspunch":1,"machpunch":1,"drainpunch":1},
+                viableMoves: {"wildcharge":1,"hijumpkick":1,"superpower":1,"thunderpunch":1,"stoneedge":1,"acrobatics":1,"bulkup":1,"substitute":1,"focuspunch":1,"machpunch":1,"drainpunch":1},
                 isNonstandard: true,
                 dreamWorldRelease: true,
                 tier: "G5CAP"
