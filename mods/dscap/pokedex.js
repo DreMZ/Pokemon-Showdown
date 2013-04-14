31dd354e447d3641b9b1dcd7434bb49604b7febe
Nollan101 <kgb@kgbudge.com>
2013-04-14 17:08:04 -0600

    Update pokedex.js

--- mods/dscap/pokedex.js
+++ mods/dscap/pokedex.js
@@ -1,3 +1,3 @@
 exports.BattlePokedex = {
-electrokan:{num:2000,species:"Electrokan",types:["Electric","Fighting"],genderRatio:{M:1,F:0},baseStats:{hp:103,atk:135,def:90,spa:55,spd:81,spe:86},abilities:{0:"Mold Breaker",DW:"Sheer Force"},heightm:4,weightkg:250,color:"Yellow",eggGroups:["Ground","Humanshape"]}
+electrokan:{num:2000,species:"Electrokan",types:["Electric","Fighting"],gender:"M",baseStats:{hp:103,atk:135,def:90,spa:55,spd:81,spe:86},abilities:{0:"Mold Breaker",DW:"Sheer Force"},heightm:4,weightkg:250,color:"Yellow",eggGroups:["Ground","Humanshape"]}
 };
