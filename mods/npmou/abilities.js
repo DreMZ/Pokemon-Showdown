91139b33305498f0216d8764e1c6bdf2cc78d35c
Nollan101 <kgb@kgbudge.com>
2013-04-14 20:04:14 -0600

    Update and rename my resolve to abilities.js

--- mods/npmou/abilities.js
+++ mods/npmou/abilities.js
@@ -1,3 +1,4 @@
 ////If you get Bk to shut up I won't shut down the serv, and I will accept your driver deal or w/e mainly because I don't need another twat annoying me in my life, and I know BK will be that twat, but I am going to try to compress the CPU usage for the connection so it doesn't lag burst my computer but keeps the serv still running well, as long as yu guys don't bring out the BK"- OI
 //kk yeah he can be really annoying sometimes can't he, thanks for understanding my position man :)
 /////your welcome as long as I can compress the cpu time we are good to go
+//lol whenever I get to adding them xD
