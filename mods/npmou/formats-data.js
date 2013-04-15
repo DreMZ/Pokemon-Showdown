3f07117c00836e5fd14e974109b54bacb697f7bc
Nollan101 <kgb@kgbudge.com>
2013-04-15 13:49:45 -0600

    Create formats-data.js

---
+++ mods/npmou/formats-data.js
@@ -0,0 +1,14 @@
+exports.BattleFormatsData = {
+        kyurem: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        kyuremblack: {
+                inherit: true,
+                tier: 'Uber'
+        },
+        thundurus: {
+                inherit: true,
+                tier: 'OU'
+        }
+};
